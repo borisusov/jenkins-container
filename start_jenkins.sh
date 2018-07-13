026d2aee1a822b888c5ff52ac7af248c81d57da6
Boris Usov <busov@ukr.net>
2018-07-14 01:19:45 +0300

    Bash statement "if" changed to "case". Remote execution added.Tested OK.

--- start_jenkins.sh
+++ start_jenkins.sh
@@ -1,44 +1,35 @@
 #!/bin/bash
+set -x
 
 #Script designed for start Project Jenkins-Container
 
 echo Please select:
-echo start - for start Project
+echo up - for start Project
 echo down - for down Project and clean-up after Project activity
 
-choice=$1
+CHOICE=$1
+HOST=$2
 
-
-
-case   "$choice" in
+#TODO: Check HOST parameter for correct IP
+case "$CHOICE" in
 	
-	"start")    docker-compose up --build -d
-
-echo
-echo "    Hello from Jenkins"
-echo "   I m working correctly"
-echo 
-echo "Number of used arguments:" $#
-echo 
-		;;
-
-	"down")    docker-compose down
-
-echo
-echo "    Good Bye from Jenkins!"
-		;;
-
-	*)	echo
-		echo "$choice is not valid input"
+ 	up) docker-compose -H "$HOST" up -d
+               echo
+               echo "    Hello from Jenkins"
+               echo "   I m working correctly"
+               echo 
+               echo "Number of used arguments:" $#
+               echo 
+	  ;;
+	down)  docker-compose -H "$HOST" down
+               echo
+               echo "    Good Bye from Jenkins!"
+          ;;
+        *)	echo
+		echo "$CHOICE is not valid input"
 	        echo "Please try again"
-
-
-
+                docker-compose -H "$HOST" ps
 esac
-exit 0
 
-
-
-
-
+exit $?
 
